6c61268f29a8abc2da966ae3f50b07ce2e3e6ed3
Ahmet Uzun <ahmet_uzun@hotmail.com>
2018-07-29 22:50:33 +0300

    Add untracked files

---
+++ AddressScraper/MainForm.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace AddressScraper
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btnGetCities = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.comboBox5 = new System.Windows.Forms.ComboBox();
+            this.comboBox6 = new System.Windows.Forms.ComboBox();
+            this.comboBox7 = new System.Windows.Forms.ComboBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(256, 130);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(334, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // btnGetCities
+            // 
+            this.btnGetCities.Location = new System.Drawing.Point(596, 130);
+            this.btnGetCities.Name = "btnGetCities";
+            this.btnGetCities.Size = new System.Drawing.Size(99, 23);
+            this.btnGetCities.TabIndex = 1;
+            this.btnGetCities.Text = "Şehirler";
+            this.btnGetCities.UseVisualStyleBackColor = true;
+            this.btnGetCities.Click += new System.EventHandler(this.btnGetCities_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(256, 166);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(334, 21);
+            this.comboBox2.TabIndex = 2;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(256, 204);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(334, 21);
+            this.comboBox3.TabIndex = 3;
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Location = new System.Drawing.Point(256, 241);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(334, 21);
+            this.comboBox4.TabIndex = 4;
+            this.comboBox4.SelectedIndexChanged += new System.EventHandler(this.comboBox4_SelectedIndexChanged);
+            // 
+            // comboBox5
+            // 
+            this.comboBox5.FormattingEnabled = true;
+            this.comboBox5.Location = new System.Drawing.Point(256, 278);
+            this.comboBox5.Name = "comboBox5";
+            this.comboBox5.Size = new System.Drawing.Size(334, 21);
+            this.comboBox5.TabIndex = 5;
+            this.comboBox5.SelectedIndexChanged += new System.EventHandler(this.comboBox5_SelectedIndexChanged);
+            // 
+            // comboBox6
+            // 
+            this.comboBox6.FormattingEnabled = true;
+            this.comboBox6.Location = new System.Drawing.Point(256, 316);
+            this.comboBox6.Name = "comboBox6";
+            this.comboBox6.Size = new System.Drawing.Size(334, 21);
+            this.comboBox6.TabIndex = 6;
+            this.comboBox6.SelectedIndexChanged += new System.EventHandler(this.comboBox6_SelectedIndexChanged);
+            // 
+            // comboBox7
+            // 
+            this.comboBox7.FormattingEnabled = true;
+            this.comboBox7.Location = new System.Drawing.Point(256, 355);
+            this.comboBox7.Name = "comboBox7";
+            this.comboBox7.Size = new System.Drawing.Size(334, 21);
+            this.comboBox7.TabIndex = 7;
+            this.comboBox7.SelectedIndexChanged += new System.EventHandler(this.comboBox7_SelectedIndexChanged);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(256, 399);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(334, 96);
+            this.richTextBox1.TabIndex = 8;
+            this.richTextBox1.Text = "";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(912, 533);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.comboBox7);
+            this.Controls.Add(this.comboBox6);
+            this.Controls.Add(this.comboBox5);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.btnGetCities);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "MainForm";
+            this.Text = "Address Scraper";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnGetCities;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.ComboBox comboBox5;
+        private System.Windows.Forms.ComboBox comboBox6;
+        private System.Windows.Forms.ComboBox comboBox7;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ AddressScraper/MainForm.cs
@@ -0,0 +1,239 @@
+﻿using HtmlAgilityPack;
+using Newtonsoft.Json;
+using ScrapySharp.Network;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AddressScraper
+{
+    public partial class MainForm : Form
+    {
+
+        ReturnData iller;
+        ReturnData ilceler;
+        ReturnData koyler;
+        ReturnData mahalleler;
+        HtmlNodeCollection caddeler;
+        HtmlNodeCollection binalar;
+        HtmlNodeCollection daireler;
+
+        public class Yt
+        {
+            public string value { get; set; }
+            public string text { get; set; }
+        }
+
+        public class ReturnData
+        {
+            public List<Yt> yt { get; set; }
+        }
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnGetCities_Click(object sender, EventArgs e)
+        {
+            ScrapingBrowser browser = new ScrapingBrowser();
+            browser.Encoding = Encoding.UTF8;
+
+            //set UseDefaultCookiesParser as false if a website returns invalid cookies format
+            //browser.UseDefaultCookiesParser = false;
+            WebPage homePage = null;
+
+            //WebPage homePage = await browser.NavigateToPageAsync(new Uri("http://adreskodu.dask.gov.tr/"));
+            //Debug.WriteLine(homePage.Html.OuterHtml);
+            homePage = await browser.NavigateToPageAsync(new Uri("http://adreskodu.dask.gov.tr/site-element/control/load.ashx"),
+                HttpVerb.Post, "fZwxqvPOpAViHEOXXVuXBaTd+2018072821lryxuVH5Y45L6Yb8Wo05CB46f1vrJzTCd1vDE8EiSLYLbaFSn0O0MhabkTx4t3A+Q==&t=il&u=0");
+            Debug.WriteLine(homePage.Html.OuterHtml);
+            iller = JsonConvert.DeserializeObject<ReturnData>(homePage.Html.OuterHtml);
+            comboBox1.Items.Clear();
+            foreach (var city in iller.yt)
+            {
+                comboBox1.Items.Add(city.text);
+            }
+            comboBox1.SelectedIndex = 0;
+
+            //HtmlNode[] resultsLinks = resultsPage.Html.CssSelect("div.sb_tlst h3 a").ToArray();
+
+            //WebPage blogPage = resultsPage.FindLinks(By.Text("romcyber blog | Just another WordPress site")).Single().Click();
+        }
+
+        private async void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var index = comboBox1.SelectedIndex;
+            if (index == 0) return;
+            var ilkodu = iller.yt[index].value;
+            var postData = "fZwxqvPOpAViHEOXXVuXBaTd+2018072821lryxuVH5Y45L6Yb8Wo05CB46f1vrJzTCd1vDE8EiSLYLbaFSn0O0MhabkTx4t3A+Q==&t=ce&u=";
+            ScrapingBrowser browser = new ScrapingBrowser();
+            browser.Encoding = Encoding.UTF8;
+            WebPage homePage = null;
+            homePage = await browser.NavigateToPageAsync(new Uri("http://adreskodu.dask.gov.tr/site-element/control/load.ashx"),
+                HttpVerb.Post, postData + ilkodu);
+            ilceler = JsonConvert.DeserializeObject<ReturnData>(homePage.Html.OuterHtml);
+            comboBox2.Items.Clear();
+            foreach (var ilce in ilceler.yt)
+            {
+                comboBox2.Items.Add(ilce.text);
+            }
+            comboBox2.SelectedIndex = 0;
+        }
+
+        private async void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var index = comboBox2.SelectedIndex;
+            if (index == 0) return;
+            var ilcekodu = ilceler.yt[index].value;
+            var postData = "fZwxqvPOpAViHEOXXVuXBaTd+2018072821lryxuVH5Y45L6Yb8Wo05CB46f1vrJzTCd1vDE8EiSLYLbaFSn0O0MhabkTx4t3A+Q==&t=vl&u=";
+            ScrapingBrowser browser = new ScrapingBrowser();
+            browser.Encoding = Encoding.UTF8;
+            WebPage homePage = null;
+            homePage = await browser.NavigateToPageAsync(new Uri("http://adreskodu.dask.gov.tr/site-element/control/load.ashx"),
+                HttpVerb.Post, postData + ilcekodu);
+            koyler = JsonConvert.DeserializeObject<ReturnData>(homePage.Html.OuterHtml);
+            comboBox3.Items.Clear();
+            foreach (var koy in koyler.yt)
+            {
+                comboBox3.Items.Add(koy.text);
+            }
+            comboBox3.SelectedIndex = 0;
+        }
+
+        private async void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var index = comboBox3.SelectedIndex;
+            if (index == 0) return;
+            var koykodu = koyler.yt[index].value;
+            var postData = "fZwxqvPOpAViHEOXXVuXBaTd+2018072821lryxuVH5Y45L6Yb8Wo05CB46f1vrJzTCd1vDE8EiSLYLbaFSn0O0MhabkTx4t3A+Q==&t=mh&u=";
+            ScrapingBrowser browser = new ScrapingBrowser();
+            browser.Encoding = Encoding.UTF8;
+            WebPage homePage = null;
+            homePage = await browser.NavigateToPageAsync(new Uri("http://adreskodu.dask.gov.tr/site-element/control/load.ashx"),
+                HttpVerb.Post, postData + koykodu);
+            mahalleler = JsonConvert.DeserializeObject<ReturnData>(homePage.Html.OuterHtml);
+            comboBox4.Items.Clear();
+            foreach (var mahalle in mahalleler.yt)
+            {
+                comboBox4.Items.Add(mahalle.text);
+            }
+            comboBox4.SelectedIndex = 0;
+        }
+
+        private async void comboBox4_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var index = comboBox4.SelectedIndex;
+            if (index == 0) return;
+            var mahallekodu = mahalleler.yt[index].value;
+            var postData = $"fZwxqvPOpAViHEOXXVuXBaTd+2018072821lryxuVH5Y45L6Yb8Wo05CB46f1vrJzTCd1vDE8EiSLYLbaFSn0O0MhabkTx4t3A+Q==&t=sf&u={mahallekodu}&term=";
+            ScrapingBrowser browser = new ScrapingBrowser();
+            browser.Encoding = Encoding.UTF8;
+            WebPage homePage = null;
+            homePage = await browser.NavigateToPageAsync(new Uri("http://adreskodu.dask.gov.tr/site-element/control/load.ashx"),
+                HttpVerb.Post, postData);
+
+            caddeler = homePage.Html.SelectNodes("//tbody/tr");
+            comboBox5.Items.Clear();
+            comboBox5.Items.Add("SEÇİNİZ");
+
+            foreach (var cadde in caddeler)
+            {
+                var hucreler = cadde.SelectNodes("td");
+                var caddeIsmi = hucreler[0].InnerText + "-" + hucreler[1].InnerText;
+                caddeIsmi = caddeIsmi.Replace("&nbsp;", " ");
+                comboBox5.Items.Add(caddeIsmi);
+            }
+            comboBox5.SelectedIndex = 0;
+        }
+
+        private async void comboBox5_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var index = comboBox5.SelectedIndex;
+            if (index == 0) return;
+            index = index - 1;
+            var caddekodu = caddeler[index].GetAttributeValue("id", "").Substring(1);
+
+            var postData = $"fZwxqvPOpAViHEOXXVuXBaTd+2018072821lryxuVH5Y45L6Yb8Wo05CB46f1vrJzTCd1vDE8EiSLYLbaFSn0O0MhabkTx4t3A+Q==&t=dk&u={caddekodu}&term=";
+            ScrapingBrowser browser = new ScrapingBrowser();
+            browser.Encoding = Encoding.UTF8;
+            WebPage homePage = null;
+            homePage = await browser.NavigateToPageAsync(new Uri("http://adreskodu.dask.gov.tr/site-element/control/load.ashx"),
+                HttpVerb.Post, postData);
+
+            binalar = homePage.Html.SelectNodes("//tbody/tr");
+            comboBox6.Items.Clear();
+            comboBox6.Items.Add("SEÇİNİZ");
+
+            foreach (var bina in binalar)
+            {
+                var hucreler = bina.SelectNodes("td");
+                var binaIsmi = hucreler[0].InnerText + "-" + hucreler[1].InnerText + "-" + hucreler[2].InnerText + "-" + hucreler[3].InnerText;
+                binaIsmi = binaIsmi.Replace("&nbsp;", " ");
+                comboBox6.Items.Add(binaIsmi);
+            }
+            comboBox6.SelectedIndex = 0;
+
+        }
+
+        private async void comboBox6_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var index = comboBox6.SelectedIndex;
+            if (index == 0) return;
+            index = index - 1;
+            var binakodu = binalar[index].GetAttributeValue("id", "").Substring(1);
+
+            var postData = $"fZwxqvPOpAViHEOXXVuXBaTd+2018072821lryxuVH5Y45L6Yb8Wo05CB46f1vrJzTCd1vDE8EiSLYLbaFSn0O0MhabkTx4t3A+Q==&t=ick&u={binakodu}&term=";
+            ScrapingBrowser browser = new ScrapingBrowser();
+            browser.Encoding = Encoding.UTF8;
+            WebPage homePage = null;
+            homePage = await browser.NavigateToPageAsync(new Uri("http://adreskodu.dask.gov.tr/site-element/control/load.ashx"),
+                HttpVerb.Post, postData);
+
+            daireler = homePage.Html.SelectNodes("//tbody/tr");
+            comboBox7.Items.Clear();
+            comboBox7.Items.Add("SEÇİNİZ");
+
+            foreach (var daire in daireler)
+            {
+                var hucreler = daire.SelectNodes("td");
+                var daireIsmi = hucreler[0].InnerText + "-" + hucreler[1].InnerText;
+                daireIsmi = daireIsmi.Replace("&nbsp;", " ");
+                comboBox7.Items.Add(daireIsmi);
+
+                Debug.WriteLine("Adres Kodu: " + daire.GetAttributeValue("id", "").Substring(1));
+
+            }
+            comboBox7.SelectedIndex = 0;
+
+        }
+
+        private async void comboBox7_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var index = comboBox7.SelectedIndex;
+            if (index == 0) return;
+            index = index - 1;
+            var dairekodu = daireler[index].GetAttributeValue("id", "").Substring(1);
+
+            var postData = $"fZwxqvPOpAViHEOXXVuXBaTd+2018072821lryxuVH5Y45L6Yb8Wo05CB46f1vrJzTCd1vDE8EiSLYLbaFSn0O0MhabkTx4t3A+Q==&t=adr&u={dairekodu}";
+            ScrapingBrowser browser = new ScrapingBrowser();
+            browser.Encoding = Encoding.UTF8;
+            WebPage homePage = null;
+            homePage = await browser.NavigateToPageAsync(new Uri("http://adreskodu.dask.gov.tr/site-element/control/load.ashx"),
+                HttpVerb.Post, postData);
+
+            var adres = homePage.Html.InnerText;
+            adres += "\r\nAdres Kodu:" + dairekodu;
+            richTextBox1.Text = adres;
+
+
+        }
+    }
+}
